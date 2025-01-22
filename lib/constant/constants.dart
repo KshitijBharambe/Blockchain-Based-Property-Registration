1d6befdf347c959d1a1cece5a586e9ab649fa36e
KSHTJ <kshitij.bharambe@gmail.com>
2025-01-22 08:58:28 -0500

    First release

---
+++ lib/constant/constants.dart
@@ -0,0 +1,12 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    '5ReEuIxsDhWxOLa2lVe9nB2PUjdEkwJ5jLsBGdBDGA.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.JuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOi';
+
+const String rpcUrl =
+    "http://127.0.0.1:7545";
+
+const String contractAddress = "0xed690C24C60A48F8A9819c9A15AD75B70CFBEa5a";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
